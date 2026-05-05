00a0018ebced17e83a00e6b6f69b9da05eff0f2b
Puech Julian <julian.puech@etu.inp-n7.fr>
2026-05-05 10:21:04 +0200

    atmospheric light

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
 import cv2 as cv
 from src.dehazer import *
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
 
 def main():
     image_path = "data/input/GT/13_outdoor_GT.jpg"
     img = cv.imread(image_path)
     img = cv.resize(img,(500,500))
-    
+
     dark_channel = compute_dark_channel(img,15)
     print(f"Type: {dark_channel.dtype}, Shape: {dark_channel.shape}")
-    cv.imshow("Dark channel", dark_channel)
-    cv.waitKey(0)
+
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+    plt.title("Image originale")
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(dark_channel, cmap='gray')
+    plt.title("Dark Channel")
+    plt.axis('off')
+
+    plt.show()
+
+    brigthest_pixels, A = estimate_atmospheric_light(img,dark_channel)
+    print(A)
+
+    plt.subplot(1, 1, 1)
+    plt.imshow(dark_channel, cmap='gray')
+    plt.title("Brightest pixels of dark channel")
+    plt.axis('off')
+
+    for _, i, j in brigthest_pixels:
+        rect = patches.Rectangle((j-0.5, i-0.5), 1, 1, linewidth=1, edgecolor='red', facecolor='none')
+        plt.gca().add_patch(rect)
+
+    plt.show()
+
+    t = transmission_estimation(img,A,0.95,15)
+
+    #Test sans soft matting
+    h,w,_ = np.shape(img)
+    test = img.copy()   
+    for c in range(3) : 
+        test[:,:,c] =  np.divide(img[:,:,c] - A[c],np.maximum(t,0.1)) + A[c]
+
+    plt.subplot(1, 1, 1)
+    plt.imshow(cv.cvtColor(test, cv.COLOR_BGR2RGB))
+    plt.title("Recovered scene radiance")
+    plt.axis('off')
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- src/dehazer.py
+++ src/dehazer.py
@@ -1,21 +1,72 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 def compute_dark_channel(image,taille_voisinage) :
     h,w,_ = np.shape(image)
-    print(h)
-    print(w)
     dark_channel = np.zeros((h,w))
-    for i in range(0,h,taille_voisinage) :
-        for j in range(0,w,taille_voisinage) :
-
+    for i in range(0,h) :
+        for j in range(0,w) :
             # calcul de l'intensité min sur le voisinage
             i_min = max(0,i-taille_voisinage//2)
             i_max = min(h,i+taille_voisinage//2)
             j_min = max(0,j-taille_voisinage//2)
             j_max = min(w,j+taille_voisinage//2)
             intensite_min = np.min(image[i_min:i_max,j_min:j_max,:])
-            dark_channel[i_min:i_max,j_min:j_max] = intensite_min
-
+            dark_channel[i,j] = intensite_min
     return dark_channel
 
 
+def estimate_atmospheric_light(image, dark_channel):
+    h,w,_ = np.shape(image)
+
+    #top 0.1% brightest pixel
+    nb_brightest = max(round(h*w*0.001), 1)
+    brightest_pixels = []
+    for i in range(0,h) :
+        for j in range(0,w) :
+            dark_channel_ij = dark_channel[i,j]
+            # a factoriser someday
+            if len(brightest_pixels)< nb_brightest :
+                brightest_pixels.append([dark_channel_ij,i,j])
+                brightest_pixels.sort(reverse=True)
+            elif (dark_channel_ij > brightest_pixels[-1][0]) :
+                # insertion dans liste triée
+                for ii in range(nb_brightest):
+                    if dark_channel_ij > brightest_pixels[ii][0]:
+                        # print("insert at : "+str(ii))
+                        brightest_pixels.insert(ii, [dark_channel_ij,i,j])
+                        break
+                brightest_pixels.pop()
+
+    #pixel with highest intensity
+    I_max = 0
+    for p in brightest_pixels :
+        c = image[p[1],p[2]]
+        I = int(c[0])+int(c[1])+int(c[2])
+        if I > I_max :
+            I_max = I
+            A = c
+
+    return brightest_pixels, A
+
+def transmission_estimation(image,A,omega,taille_voisinage):
+    h,w,_ = np.shape(image)
+    t = np.zeros((h,w))
+    nb_patches_h = math.ceil(h/taille_voisinage)
+    nb_patches_w = math.ceil(w/taille_voisinage)
+    for p_i in range(nb_patches_h) :
+        for p_j in range(nb_patches_w) :
+            y_min = -1
+            for c in range(3) :
+                i_min = p_i*taille_voisinage
+                i_max = min((p_i+1)*taille_voisinage,h)
+                j_min = p_j*taille_voisinage
+                j_max = min((p_j+1)*taille_voisinage,w)
+                y = np.min(image[i_min:i_max,j_min:j_max,c])
+                y_c = y/A[c]
+                if y_c < y_min or y_min == -1 :
+                    y_min = y_c
+            t[p_i*taille_voisinage:min((p_i+1)*taille_voisinage,h),p_i*taille_voisinage:min((p_j+1)*taille_voisinage,w)] = 1-omega*y_min
+    
+    return t
